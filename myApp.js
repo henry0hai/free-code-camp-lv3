515a04f2c10453320df60df611464cc95a7eee85
henry <henry0hai@gmail.com>
2023-05-29 18:11:20 +0700

    feat: find one by food

--- myApp.js
+++ myApp.js
@@ -60,7 +60,14 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food}, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+      done(null , data);
+    }
+  })
 };
 
 const findPersonById = (personId, done) => {
